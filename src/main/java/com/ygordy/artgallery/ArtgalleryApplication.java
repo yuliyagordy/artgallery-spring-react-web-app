d5117c6d161aaa018de4eac5ebca0514c3956692
yuliyagordy <yuliyagordy@gmail.com>
2020-10-26 17:39:08 -0400

    initial project commit

---
+++ src/main/java/com/ygordy/artgallery/ArtgalleryApplication.java
@@ -0,0 +1,13 @@
+package com.ygordy.artgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtgalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtgalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygordy/artgallery/ArtgalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygordy.artgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtgalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
