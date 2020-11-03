7fee257151f68763755247528e85fdc771ca912a
yuliyagordy <yuliyagordy@gmail.com>
2020-11-03 09:46:39 -0500

    main app class has been renamed

--- src/main/java/com/ygordy/artgallery/ArtGalleryApplication.java
+++ src/main/java/com/ygordy/artgallery/ArtGalleryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArtgalleryApplication {
+public class ArtGalleryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArtgalleryApplication.class, args);
+		SpringApplication.run(ArtGalleryApplication.class, args);
 	}
 
 }
